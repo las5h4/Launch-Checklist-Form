c353917a1e43b8bb8333a989e59bc422277f8941
las5h4 <loganasmalley@gmail.com>
2019-12-16 19:39:02 -0600

    Figured it out! Thanks Joe! Had to move preventDefault to begining of event listener

--- script.js
+++ script.js
@@ -14,19 +14,27 @@
 window.addEventListener("load", function(){
    let form = document.getElementById("launchForm")
    form.addEventListener("submit", function(event){
+      event.preventDefault()
       let pilotNameInput = document.querySelector("input[name=pilotName]")
       let copilotNameInput = document.querySelector("input[name=copilotName]")
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]")
       let cargoMassInput = document.querySelector("input[name=cargoMass]")      
       if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
          alert("All fields required.")
-         event.preventDefault()         
+         // event.preventDefault()         
       } else if (isNaN(pilotNameInput.value) === false || isNaN(copilotNameInput.value) === false || isNaN(fuelLevelInput.value) === true || isNaN(cargoMassInput.value) === true) {
          alert("One or more inputs are invalid.")
-         event.preventDefault()
-      } else if (fuelLevelInput.value < 10000) {
+         // event.preventDefault()
+      }
+      if (Number(fuelLevelInput.value) < 10000) {
+         let faultyItems = document.getElementById("faultyItems")
          faultyItems.style.visibility = "visible"
          console.log("hello")
       } 
    })
+   // let formSubmit = document.getElementById("formSubmit")
+   // formSubmit.addEventListener("click", function(){
+   //    let faultyItems = document.getElementById("faultyItems")
+   //    faultyItems.style.visibility = "visible"
+   // })
 })
